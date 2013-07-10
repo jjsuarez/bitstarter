e6025826ae235f411da6dc93f67c6521814ce18f
Jose Juan Suarez <josejs@hotmail.com>
2013-07-10 15:22:56 -0430

    Update read index.html

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World2!');
+app.get('/', function(request, response)
+{
+    var fs = require('fs');
+    var input = fs.readFileSync('index.html','utf8');
+    console.log(input);
+    response.send(input);
 });
 
 var port = process.env.PORT || 5000;
