6155fef6fd6f068822cfef196f108a0eec345ca3
Jose Juan Suarez <josejs@hotmail.com>
2013-07-12 01:45:26 -0430

    HW3 submitted grader checks

--- grader.js
+++ grader.js
@@ -22,11 +22,14 @@ References:
 */
 
 var fs = require('fs');
+var util = require('util');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-
+var URL_DEFAULT = "http://protected-garden-7576.herokuapp.com";
+var WEBHTML="webindex.html"
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
@@ -63,12 +66,33 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists))
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.option('-u, --url <url_location>', 'URL of index.html', URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if(program.file===undefined)
+    {
+	rest.get(program.url).on('complete', function(result) {
+	    if (result instanceof Error) {
+		util.puts('Error: ' + result.message);
+		this.retry(5000); // try again after 5 sec
+	    } else {
+		//util.puts(result);
+		fs.writeFileSync(WEBHTML, result);
+		var checkJson = checkHtmlFile(WEBHTML, program.checks);
+		var outJson = JSON.stringify(checkJson, null, 4);
+		console.log(outJson);
+		fs.writeFileSync("outJson", outJson);
+	    }
+	}); 
+
+    }
+    else
+    {
+	var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
